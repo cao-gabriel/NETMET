b20ce94f2d844c7dc6348658cc2b2212c923d779
Cao Gabriel <cao.gabriel.367@gmail.com>
2021-11-14 00:04:33 +0100

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   fingerprint.py
    	deleted:    ping.pcap

--- labs/5/fingerprint.py
+++ labs/5/fingerprint.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Created on Sat Nov 13 18:23:09 2021
+
+@author: root
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
 Created on Wed Nov 10 13:15:35 2021
 
 @author: root
@@ -12,60 +20,120 @@ from scapy.sendrecv import sr1
 from json import dumps
 MAX_TTL = 30
 TIMEOUT = 3
-destinations = ["128.104.159.131", "128.104.159.130", "128.104.159.128"]
+DESTINATIONS = ["128.104.159.131", "128.104.159.130", "128.104.159.128"]
 
 
-def get_ips(destinations, my_database):
+def time_exceeded_ttl(destinations):
     '''
-    Groups ip routers coming from the same router
+    Execute a traceroute on each ip adress in the given parameter 'destinations'
+    and retrieve the ip adress and the ttl in the packet sent by each router of
+    the path. If a router doesn't send back a packet, the router will be ignored.
 
     Parameters
     ----------
-    ip_routers : string list
-        List of destinations.
+    destinations : List of string
+        A list containing IP adresses that will be tracerouted.
 
     Returns
     -------
-    None.
+    ip_traceroute_ttl : Dictionary
+        A dictionary which keys are the IP adresses of routers and values are
+        the TTL in the Time Exceeded packet sent by a router during a traceroute.
+        If a router didn't answer a probe, it will not be in the dicitonary.
 
     '''
-    routers_ip = set()
+    ip_traceroute_ttl = {} # key = ip value = traceroute ttl
     for destination in destinations:
-        for ttl in range(1, MAX_TTL + 1):
-            print(ttl, end = ' ')
-            request = IP(dst=destination, ttl=ttl) / ICMP()
-            response = sr1(request, timeout=TIMEOUT, verbose=False)
-            if response is None:
-                print( "*")
-                continue
-            else:
-                routers_ip.add((response[IP].src, response[IP].ttl))
-                print(response[IP].src)
-            if response[ICMP].type == 0:
-                break
-                #response.show()
-    return routers_ip
+        ttl = 1
+        reply = False
+        print(f"Traceroute to {destination}")
+        while ttl <= MAX_TTL and not reply:
+            traceroute_probe = IP(dst = destination, ttl = ttl) / ICMP()
+            probe_response = sr1(traceroute_probe, timeout = TIMEOUT, verbose = False)
+            if probe_response is not None:
+                reply = probe_response[ICMP] == 0
+                if not reply:
+                    router_ip = probe_response[IP].src
+                    router_ttl = probe_response[IP].ttl
+                    print("\t IP = {: <22} TTL = {}".format(router_ip, router_ttl))
+                    ip_traceroute_ttl[router_ip] = router_ttl
+            ttl += 1
+            
+    return ip_traceroute_ttl
 
-def build_database(routers_ip, my_database):
-    for ip, traceroute_ttl in routers_ip:
-        my_ping_request = IP(dst = ip) / ICMP()
-        my_ping_response = sr1(my_ping_request, timeout=TIMEOUT, verbose = False)
-        if my_ping_response is not None:
-            ping_ttl = my_ping_response[IP].ttl
-            print(ip, traceroute_ttl, ping_ttl)
-            if ping_ttl not in my_database:
-                my_database[ping_ttl] = {}
-                my_database[ping_ttl][traceroute_ttl] = [ip]
-            else:
-                if traceroute_ttl in my_database:
-                    my_database[ping_ttl][traceroute_ttl].append(ip)
-                else:
-                    my_database[ping_ttl][traceroute_ttl] = [ip]
-    
+def echo_reply_ttl(ip_list):
+    '''
+    Execute a ping to each IP adress in the given parameter ip_list and retrieve
+    the TTL in the echo reply packet sent by the machine identified by this ip adress.
+    If no packet is sent back by the machine. The machine will be ignored.
+
+    Parameters
+    ----------
+    ip_list : List of string
+        A list containing IP adresses that will be pinged.
+
+    Returns
+    -------
+    ip_ping_ttl : Dictionary
+        A dictionary which keys are the IP adresses of machines and values are
+        the TTL in the ECHO Reply packet sent by the machine during a PING. If
+        a machine didn't respond to a PING, it will not be in the dictionary.
+
+    '''
+    ip_ping_ttl = {}
+    print("Ping")
+    for ip in ip_list:  
+        ping = IP(dst = ip) / ICMP()
+        ping_response = sr1(ping, timeout = TIMEOUT, verbose = False)
+        if ping_response is not None:
+            router_ttl = ping_response[IP].ttl
+            ip_ping_ttl[ip] = router_ttl
+            print("\t IP = {: <22} TTL = {}".format(ip, router_ttl))
+    return ip_ping_ttl
+
+def build_database(my_time_exceeded_ttl, my_echo_reply_ttl):
+    '''
+    Classify IP adresses present in both given parameters according to their TTL
+    signature
+
+    Parameters
+    ----------
+    my_time_exceeded_ttl : Dictionary
+        A dictionary returned by the function time_exceeded_ttl.
+    my_echo_reply_ttl : TYPE
+        A dictionary returned by the function echo_reply_ttl.
+
+    Returns
+    -------
+    my_database : Dictionary of dictionary of lists
+        This is a database classifying IP adresses according to their TTL signature.
+        The keys of the dictionaries value of TTLs. For instance, my_database[40][40]
+        will give all IP adresses with the TTL signature (40,40)
+
+    '''
+    my_database = {}
+    intersection_ip = set(my_time_exceeded_ttl.keys()).intersection(set(my_echo_reply_ttl.keys()))
+    for ip in intersection_ip:
+        traceroute_ttl = my_time_exceeded_ttl[ip]
+        ping_ttl = my_echo_reply_ttl[ip]
+        if ping_ttl not in my_database:
+            my_database[ping_ttl] = {}
+            my_database[ping_ttl][traceroute_ttl] = [ip]
         else:
-            print("*")
-        
+            if traceroute_ttl in my_database[ping_ttl]:
+                my_database[ping_ttl][traceroute_ttl].append(ip)
+            else:
+                my_database[ping_ttl][traceroute_ttl] = [ip]
     return my_database
+
 #%% Running
-my_database = {}
-print(dumps(build_database(get_ips(destinations, my_database), my_database), indent= 4))
+if __name__ == "__main__":
+    my_time_exceeded_ttl = time_exceeded_ttl(DESTINATIONS)
+    my_echo_reply_ttl = echo_reply_ttl(my_time_exceeded_ttl.keys())
+    my_database = build_database(my_time_exceeded_ttl, my_echo_reply_ttl)
+    for ping_ttl in my_database:
+        for traceroute_ttl in my_database[ping_ttl]:
+            print("({:<3}, {:<3}) : ".format(ping_ttl, traceroute_ttl), end="|")
+            for ip in my_database[ping_ttl][traceroute_ttl]:
+                print("{:<16}".format(ip), end = "")
+            print()
